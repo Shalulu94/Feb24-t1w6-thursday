dbd0d6281ff2cccd2170764cb5fcd91307e8da87
shalulu94 <shahrulnasir290@hotmail.com>
2024-04-04 20:23:11 +1100

    else conditions

---
+++ marks.py
@@ -0,0 +1,20 @@
+# if, else if ladder
+# <80 -> distinction
+# 60-80 => first division
+# 40-60 -> second division
+# <40 -> fail
+
+marks = 90
+
+if marks >= 80:
+    print("Distinction")
+elif marks >= 60:
+    print("First Division")
+elif marks >= 40:
+    print("Second Division")
+else:
+    print("Fail")
+
+
+
+    
---
+++ raining.py
@@ -0,0 +1,16 @@
+# Conditional statements
+# statements tat allow us to execute statements
+# based on some conditions
+
+# If/Else
+
+is_raining = True
+
+if is_raining:
+    print("It is raining")
+    print("I need an umbrella")
+else:
+    print("It is not raining")
+    print("I don't need an umbrella")
+
+
