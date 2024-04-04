9da293284e5a4d7ff8714361ffd037862e60383f
shalulu94 <shahrulnasir290@hotmail.com>
2024-04-04 20:57:23 +1100

    Added in challenge for if, elif, elif ladder

---
+++ nested_if_else.py
@@ -0,0 +1,25 @@
+# 2 states - State1 and State2 - take input from the user
+state = input("Enter your state: ")
+
+# take age as input from the user
+age = int(input("Enter your age: "))
+
+# In State1, 
+# age >= 18 can vote
+# age < 18 cannot vote
+
+# In State2
+# age >= 20 can vote
+# age < 20 cannot vote
+
+if state == "State1":
+    if age >= 18:
+        print("can vote")
+    else:
+        print("cannot vote")
+else:
+    if age >= 20:
+        print("can vote")
+    else:
+        print("cannot vote")
+
